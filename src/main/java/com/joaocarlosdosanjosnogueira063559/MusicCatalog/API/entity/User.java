85bd354ddc6d90f58d89ea05a3c1e4f015032fc0
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2026-01-27 23:19:54 -0400

    refactor: adiciona getters e setters na entidade User

--- src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/entity/User.java
+++ src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/entity/User.java
@@ -28,6 +28,42 @@ public class User implements UserDetails {
 
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public User(Long id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
