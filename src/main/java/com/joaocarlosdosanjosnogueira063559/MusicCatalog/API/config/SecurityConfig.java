cd64369f42d91b85dcc5780cf4c045430795191e
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2026-02-02 14:57:36 -0400

    fix: liberar verbo PATCH bloqueado pela política de CORS

--- src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/config/SecurityConfig.java
+++ src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
 
