626620b7aada7797e8e66e131f75a0363a1b4141
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2026-02-02 15:11:35 -0400

    feat: configurar perfil de teste ativo em MusicCatalogApiApplicationTests

--- src/test/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/MusicCatalogApiApplicationTests.java
+++ src/test/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/MusicCatalogApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.joaocarlosdosanjosnogueira063559.MusicCatalog.API;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MusicCatalogApiApplicationTests {
 
 	@Test
