addf0db3745566a73d4c8432a01a12832fe3e47f
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2026-02-03 16:04:46 -0400

    refactor(security): mudar UserDetailsService para injeção de campo com @Lazy

--- src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/security/JwtAuthenticationFilter.java
+++ src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/security/JwtAuthenticationFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,11 +21,13 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
 
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+    @Autowired
+    @Lazy
+    private UserDetailsService userDetailsService;
+
+    public JwtAuthenticationFilter(JwtService jwtService) {
         this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
     }
 
     @Override
