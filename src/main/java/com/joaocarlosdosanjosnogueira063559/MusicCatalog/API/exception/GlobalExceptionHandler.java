653deffdc5de2a6c4d677e324650a9fa3f5d4e60
JoaoCarlosNogueira <joaocarlos2002_br@hotmail.com>
2026-02-04 16:11:57 -0400

    refactor(exception): centralizar tratamento global de erros em pacote dedicado

--- src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/exception/GlobalExceptionHandler.java
+++ src/main/java/com/joaocarlosdosanjosnogueira063559/MusicCatalog/API/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.joaocarlosdosanjosnogueira063559.MusicCatalog.API.config.web;
+package com.joaocarlosdosanjosnogueira063559.MusicCatalog.API.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
